c8925d14fac9000efe5d842763849acb667b77c5
yri kramar <y.kramar@gpsolutions.com>
2017-09-28 16:42:59 +0300

    config for build war for websphere. but with this config I cant start app in my IDEA

--- src/main/java/com/kramar/Application.java
+++ src/main/java/com/kramar/Application.java
@@ -4,16 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application extends SpringBootServletInitializer implements CommandLineRunner  {
 
     @Autowired
     PasswordEncoder encoder;
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
     @Override
--- src/main/java/com/kramar/config/CommonConfig.java
+++ src/main/java/com/kramar/config/CommonConfig.java
@@ -3,17 +3,14 @@ package com.kramar.config;
 import com.kramar.data.service.SpringSecurityAuditorAware;
 import com.kramar.data.util.AutowireHelper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
 import java.util.UUID;
 
 @Configuration
