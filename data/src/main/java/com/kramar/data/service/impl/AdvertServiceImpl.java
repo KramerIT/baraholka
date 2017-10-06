2e30d0db2cd91f2d003c90284203a31d924dbcb2
yri kramar <y.kramar@gpsolutions.com>
2017-10-06 17:11:12 +0300

    Add javadoc

--- data/src/main/java/com/kramar/data/service/impl/AdvertServiceImpl.java
+++ data/src/main/java/com/kramar/data/service/impl/AdvertServiceImpl.java
@@ -18,6 +18,9 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+/**
+ * Service layer class to wort with adverts
+ */
 @Service
 @Profiling
 public class AdvertServiceImpl implements AdvertService {
@@ -29,23 +32,41 @@ public class AdvertServiceImpl implements AdvertService {
     @Autowired
     private AuthenticationService authenticationService;
 
+    /**
+     * Get all adverts with pagination
+     * @param pageable class with pagination information
+     * @return advert with pagination
+     */
     @Override
     public Page<AdvertDto> getAllAdverts(final Pageable pageable) {
         return advertRepository.findAll(pageable).map(advertConverter::transform);
     }
 
+    /**
+     * Get all adverts by status
+     * @param advertStatus advert status
+     * @return list adverts
+     */
     @Override
     public List<AdvertDto> getAllAdvertsByStatus(final AdvertStatus advertStatus) {
         final List<AdvertDbo> byAdvertStatus = advertRepository.findByAdvertStatus(advertStatus);
         return byAdvertStatus.stream().map(advertConverter::transform).collect(Collectors.toList());
     }
 
+    /** Get all adverts with pagination by current user in session
+     * @param pageable class with pagination information
+     * @return advert with pagination
+     */
     @Override
     public Page<AdvertDto> getAllAdvertsByUser(final Pageable pageable) {
         final UserDbo user = authenticationService.getCurrentUser();
         return advertRepository.findByOwner(user, pageable).map(advertConverter::transform);
     }
 
+    /** Create new advert
+     * @param advertDto new advert
+     * @return saved advert
+     */
     @Override
     public AdvertDto createAdvert(final AdvertDto advertDto) {
         advertDto.setId(null);
@@ -54,18 +75,32 @@ public class AdvertServiceImpl implements AdvertService {
         return advertConverter.transform(advertDbo);
     }
 
+    /**
+     * Get advert by id
+     * @param id advert id
+     * @return advert
+     */
     @Override
     public AdvertDto getAdvertById(final UUID id) {
         final AdvertDbo advertDbo = advertRepository.getById(id);
         return advertConverter.transform(advertDbo);
     }
 
+    /** Delete advert by id
+     * @param id advert id
+     */
     @Override
     public void deleteAdvertById(final UUID id) {
         final AdvertDbo advertDbo = advertRepository.getById(id);
         advertRepository.delete(advertDbo);
     }
 
+    /**
+     * Modify advert
+     * @param id advert id
+     * @param advertDto advert to modify
+     * @return modified advert
+     */
     @Override
     public AdvertDto modifyAdvertById(final UUID id, final AdvertDto advertDto) {
         final AdvertDbo oldAdvert = advertRepository.getById(id);
